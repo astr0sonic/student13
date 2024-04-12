bebc3e8455b7480f98f3d3e771799bdcaa37de50
Deenaism24 <160231053+Deenaism24@users.noreply.github.com>
2024-04-12 21:48:59 +0300

    task6

---
+++ rpn.cpp
@@ -0,0 +1,35 @@
+#include "rpn.h"
+#include <stack>
+
+double calculateRPN(const std::string& expr) {
+    std::stack<double> st;
+    double num;
+    char k, lst;
+    int i = 0;
+    while (i < std::size(expr))
+    {
+        k = expr[i];
+        i++;
+        if (isdigit(k))
+            st.push(k - '0');
+        else if (k == '+' || k == '-' || k == '*' || k == '^' || k == '/')
+        {
+            lst = k;
+            num = st.top();
+            st.pop();
+            if (k == '+')
+                num += st.top();
+            else if (k == '-')
+                num = st.top() - num;
+            else if (k == '*')
+                num *= st.top();
+            else if (k == '/')
+                num = st.top() / num;
+            else if (k == '^')
+                num = pow(st.top(), num);
+            st.pop();
+            st.push(num);
+        }
+    }
+    return st.top();
+}
