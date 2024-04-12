1aa0a1f287c19d1311305b7dbe393d06f898fb03
Deenaism24 <160231053+Deenaism24@users.noreply.github.com>
2024-04-12 21:39:35 +0300

    task5

---
+++ bracket.cpp
@@ -0,0 +1,33 @@
+#include "bracket.h"
+
+bool checkBrackets(const std::string& s) {
+    std::stack<int> br;
+    for (int i = 0; i < size(s); i++)
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[' || s[i] == '<')
+            br.push(s[i]);
+        else if (s[i] == ')')
+        {
+            if (size(br) == 0 || br.top() != '(')
+                return false;
+            br.pop();
+        }
+        else if (s[i] == '}')
+        {
+            if (size(br) == 0 || br.top() != '{')
+                return false;
+            br.pop();
+        }
+        else if (s[i] == ']')
+        {
+            if (size(br) == 0 || br.top() != '[')
+                return false;
+            br.pop();
+        }
+        else if (s[i] == '>')
+        {
+            if (size(br) == 0 || br.top() != '<')
+                return false;
+            br.pop();
+        }
+    return true;
+}
